b5888d2fb50bca0c7500dd21ed349aa31ade8c5c
Ishita Agarwal <67784472+IshitaAgarwal13@users.noreply.github.com>
2021-09-26 18:57:45 +0530

    Create maxofThree.js
    2. Define a method to find max of 3 numbers.

---
+++ maxofThree.js
@@ -0,0 +1,13 @@
+function max(a,b,c){
+    var max=0;
+    if(a>max)
+      max=a;
+    if(b>max)
+      max=b;
+    if(c>max)
+      max=c;
+      
+    return max;  
+}
+
+console.log(max(5,12,7));
